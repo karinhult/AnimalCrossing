f2c9f35a85eac3d29df18562218ef460e05be1a9
Ida Ekmark <ekmark@student.chalmers.se>
2021-12-11 14:25:51 +0100

    Small change

--- SugarscapeEx.py
+++ SugarscapeEx.py
@@ -115,7 +115,7 @@ runs = 20
 hasRoad = True
 oneSide = True
 hasCrossings = True
-bridgeIndices = np.array([]).astype(int)
+tunnelIndices = np.array([]).astype(int)
 '''
 np.array([L/2-1, L/2]).astype(int)
 np.array([L/3-1, L/3, 2*L/3, 2*L/3+1]).astype(int)
@@ -124,8 +124,7 @@ np.array([L/6-1, L/6, 2*L/6-1, 2*L/6, 3*L/6-1, 3*L/6, 4*L/6, 4*L/6+1, 5*L/6, 5*L
 np.array([L/11-1, L/11, 2*L/11-1, 2*L/11, 3*L/11-1, 3*L/11, 4*L/11-1, 4*L/11, 5*L/11-1, 5*L/11, 
             6*L/11, 6*L/11+1, 7*L/11, 7*L/11+1, 8*L/11, 8*L/11+1, 9*L/11, 9*L/11+1, 10*L/11, 10*L/11+1]).astype(int)
 '''
-tunnelIndices = np.array([L/11-1, L/11, 2*L/11-1, 2*L/11, 3*L/11-1, 3*L/11, 4*L/11-1, 4*L/11, 5*L/11-1, 5*L/11,
-            6*L/11, 6*L/11+1, 7*L/11, 7*L/11+1, 8*L/11, 8*L/11+1, 9*L/11, 9*L/11+1, 10*L/11, 10*L/11+1]).astype(int)
+bridgeIndices = np.array([L/3-1, L/3, 2*L/3, 2*L/3+1]).astype(int)
 saveDataToFile = True
 animateSimulation = False
 
